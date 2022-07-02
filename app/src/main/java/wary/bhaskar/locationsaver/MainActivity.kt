95ce03e404badf183327d5e01793506fceb16c2e
Bhaskar100-hub <bhaskarwary7@gmail.com>
2022-07-02 20:06:41 +0530

    Displaying Map with Markers

--- app/src/main/java/wary/bhaskar/locationsaver/MainActivity.kt
+++ app/src/main/java/wary/bhaskar/locationsaver/MainActivity.kt
@@ -1,12 +1,18 @@
 package wary.bhaskar.locationsaver
 
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import wary.bhaskar.locationsaver.models.Place
 import wary.bhaskar.locationsaver.models.UserMap
 
+const val EXTRA_USER_MAP = "EXTRA_USER_MAP"
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,7 +21,19 @@ class MainActivity : AppCompatActivity() {
         val userMaps = generateSampleData()
 
         rvMaps.layoutManager = LinearLayoutManager(this)
-        rvMaps.adapter = MapsAdapter(this, userMaps)
+        rvMaps.adapter = MapsAdapter(this, userMaps, object: MapsAdapter.OnClickListener {
+            override fun onItemClick(position: Int) {
+                Log.i(TAG, "onItemClick $position")
+                val intent = Intent(this@MainActivity, DisplayMapActivity::class.java)
+                intent.putExtra(EXTRA_USER_MAP, userMaps[position])
+                startActivity(intent)
+            }
+
+            override fun onItemLongClick(position: Int) {
+                Log.i(TAG, "onItemLongClick at position $position")
+            }
+
+        })
     }
 
     private fun generateSampleData(): List<UserMap> {
